4694ccb04d16dbf338b162ff7daeec0a980f8515
Darren Clark <darclar@ut.utm.edu>
2022-10-13 13:14:07 -0500

    created repo.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Darren Clark
+ * @date 2022-10-13
+ * @brief Program to function like a phone book or contacts app.
+ * 
+ * Program that sorts, stores, names and phone numbers. The user can also perform many actions on the entries.
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "Hello." << endl;
+  
+  
+  return 0;
+}
---
+++ phonebook.h
@@ -0,0 +1,8 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+
+
+
+#endif //PHONEBOOK_H
+
